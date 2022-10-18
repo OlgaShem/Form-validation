a08d29f5841ab2b9e2cba065431b610d5256e314
Olga <43566846+OlgaShem@users.noreply.github.com>
2022-10-18 16:53:00 +0300

    Add files

---
+++ script.js
@@ -0,0 +1,88 @@
+//Payment tabs 
+function openPay(evt, payName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(payName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+//Form validation
+$("#validate").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      name: {
+        required: true,
+        minlength: 2
+      },
+     number: {
+        digits: true,
+        minlength: 6
+     },
+     code: {
+      digits: true,
+      minlength: 3
+    },
+    card: {
+      digits: true,
+      minlength: 16
+    }
+  },
+    messages: {
+      email: {
+        required: "Write your email",
+        email: "Your email must be in the format of name@domain.com"
+      },
+      name: {
+        required: "Write your name",
+        minlength: jQuery.validator.format("Name should be more than 1 symbols"),
+      }
+    },
+  });
+
+ //Hide input label
+$('input.name').focus(function(){
+    $('label.label-name').hide();
+});
+
+$('input.surname').focus(function(){
+    $('label.label-surname').hide();
+});
+
+$('input.secur').focus(function(){
+    $('label.label-secur').hide();
+});
+
+$('input.email').focus(function(){
+  $('label.label-email').hide();
+});
+
+$('input.card').focus(function(){
+  $('label.label-card').hide();
+});
+
+//Dropdown with flags
+function showMe(evt) {
+  console.log("evt.value ",evt.value);
+}
+function makeDd() {
+  'use strict';
+  let json = new Function(`return ${document.getElementById('json_data').innerHTML}`)();
+
+  MsDropdown.make("#json_dropdown", {
+    byJson: {
+      data: json, selectedIndex:0
+    }
+  });
+}
